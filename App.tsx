7cbe6a974c22ba0b3ff9710c7c96b3ca5ca21ec1
Aryan Jabbari <AryanJabbari@gmail.com>
2024-03-09 19:23:33 -0500

    provide workaround for a locked Satscard

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@ import * as Clipboard from 'expo-clipboard';
 export default function App() {
   const card = useRef(new CKTapCard()).current;
   const [cvc, setCvc] = useState('');
+  const [authDelay, setAuthDelay] = useState(0);
+  const [isBusy, setIsBusy] = useState(false);
 
   useEffect(() => {
     const endSession = async () => {
@@ -22,7 +24,43 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Place your phone atop your SatsCard while using this app.</Text>
+      <View style={styles.instructionsContainer}>
+        <Text>1. Enter your CVC then press the button below.</Text>
+        <Text>2. Place your phone atop your SatsCard.</Text>
+        <Text>3. Copy the import string into freewallet.io</Text>
+      </View>
+      {authDelay > 0 && (
+        <>
+          <Text>
+            You are locked out for {authDelay} seconds. Keep phone on card and press "Wait" button
+            below to unlock.
+          </Text>
+          <TouchableOpacity
+            disabled={isBusy}
+            onPress={async () => {
+              setIsBusy(true);
+              try {
+                await card.nfcWrapper(async () => {
+                  while (authDelay > 0) {
+                    await card.wait();
+                    const status = await card.get_status();
+                    setAuthDelay(status.auth_delay ?? 0);
+                  }
+                });
+              } catch (e) {
+                if (e && e instanceof Error) {
+                  ToastAndroid.showWithGravity(e.message, ToastAndroid.SHORT, ToastAndroid.TOP);
+                }
+              } finally {
+                setIsBusy(false);
+              }
+            }}
+            style={[styles.button, isBusy && { opacity: 0.5 }]}
+          >
+            <Text>Wait</Text>
+          </TouchableOpacity>
+        </>
+      )}
       <Text>Enter your CVC below:</Text>
       <TextInput
         keyboardType="number-pad"
@@ -30,25 +68,41 @@ export default function App() {
         style={styles.textInput}
       />
       <TouchableOpacity
+        disabled={isBusy}
         onPress={async () => {
-          const address = await card.nfcWrapper(async () => {
-            // interact with the card here
-            try {
-              await card.first_look();
-              const privateKey = await card.get_privkey(cvc, 0); // scans the card for basic details and initialises with it
+          setIsBusy(true);
+          try {
+            await card.nfcWrapper(async () => {
+              const status = await card.first_look();
+              if (status.auth_delay) {
+                setAuthDelay(status.auth_delay ?? 0);
+                return status;
+              }
+              if (card.active_slot === 0) {
+                const slot0 = await card.get_slot_usage(0);
+                if (slot0.status === 'SEALED') {
+                  await card.unseal_slot(cvc);
+                }
+              }
+              const privateKey = await card.get_privkey(cvc, 0);
               const wifKey = wif.encode(128, privateKey, true);
               await Clipboard.setStringAsync(`p2wpkh:${wifKey}`);
               ToastAndroid.show('Freewallet import string copied to clipboard', ToastAndroid.SHORT);
-              return wifKey;
-            } catch (e) {
-              if (e instanceof Error) {
-                ToastAndroid.show(e.message, ToastAndroid.SHORT);
+            });
+          } catch (e) {
+            if (e) {
+              if (e instanceof Error && typeof e.message === 'string') {
+                ToastAndroid.showWithGravity(e.message, ToastAndroid.SHORT, ToastAndroid.TOP);
               }
-
-              console.error(e, typeof e);
+              if (typeof e === 'string') {
+                ToastAndroid.showWithGravity(e, ToastAndroid.SHORT, ToastAndroid.TOP);
+              }
             }
-          });
+          } finally {
+            setIsBusy(false);
+          }
         }}
+        style={[styles.button, isBusy && { opacity: 0.5 }]}
       >
         <Text>Copy Freewallet import string to clipboard</Text>
       </TouchableOpacity>
@@ -58,10 +112,19 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
+    marginHorizontal: 20,
   },
-  textInput: { borderWidth: 1, width: '100%', height: 50 },
+  button: {
+    backgroundColor: '#ddd5f3',
+    borderRadius: 4,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginTop: 24,
+  },
+  instructionsContainer: { marginBottom: 32, rowGap: 8 },
+  textInput: { borderWidth: 1, paddingHorizontal: 16, height: 50, width: '100%' },
 });
